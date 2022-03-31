d63dec23c2a5586f21abd6ae189bda92bfa532e7
Excell27 <excell@nutrifood.co.id>
2022-03-31 10:29:05 +0700

    belum ke login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
+    
   end
 
   # GET /users/1 or /users/1.json
   def show
-    @articles = set_user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    debugger
   end
 
   # GET /users/new
